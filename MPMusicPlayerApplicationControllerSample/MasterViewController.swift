171dfc35fa3b466717f93326c09f220e730f2797
Shingo Hiraya <shingohiraya@gmail.com>
2017-01-27 17:44:04 +0900

    Add picker and player implementation

--- MPMusicPlayerApplicationControllerSample/MasterViewController.swift
+++ MPMusicPlayerApplicationControllerSample/MasterViewController.swift
@@ -7,36 +7,83 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class MasterViewController: UITableViewController {
-    var objects = [Any]()
+    let musicPlayerApplicationController = MPMusicPlayerController.applicationQueuePlayer()
+    var mediaItems: [MPMediaItem] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set up navigationItem
         navigationItem.leftBarButtonItem = editButtonItem
-
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         navigationItem.rightBarButtonItem = addButton
+
+        // set up toolbar
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                       target: nil,
+                                       action: nil)
+        let previous = UIBarButtonItem(barButtonSystemItem: .rewind,
+                                       target: self,
+                                       action: #selector(didTapPrevButton(_:)))
+        let next = UIBarButtonItem(barButtonSystemItem: .fastForward,
+                                   target: self,
+                                   action: #selector(didTapNextButton(_:)))
+        toolbarItems = [
+            flexible,
+            previous,
+            flexible,
+            flexible,
+            next,
+            flexible
+        ]
+        navigationController?.setToolbarHidden(false, animated: true)
+        
+        // check Authorization
+        MPMediaLibrary.requestAuthorization { status in
+            print("status:", status == .authorized ? "authorized" : "Not authorized")
+        }
+    }
+    
+    func didTapPrevButton(_ sender: Any) {
+        musicPlayerApplicationController.skipToPreviousItem()
+    }
+    
+    func didTapNextButton(_ sender: Any) {
+        musicPlayerApplicationController.skipToNextItem()
     }
 
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+        // present MPMediaPickerController
+        let picker = MPMediaPickerController(mediaTypes: .music)
+        picker.allowsPickingMultipleItems = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func setQueueAndPlay(mediaItemCollection: MPMediaItemCollection) {
+        mediaItems = mediaItemCollection.items
+        tableView.reloadData()
+        
+        // setQueue and play
+        musicPlayerApplicationController.setQueue(with: mediaItemCollection)
+        musicPlayerApplicationController.play()
     }
 }
 
 // MARK: - UITableViewDataSource
 extension MasterViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return mediaItems.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let item = mediaItems[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.artist
         return cell
     }
 }
@@ -44,16 +91,34 @@ extension MasterViewController {
 // MARK: - UITableViewDelegate
 extension MasterViewController {
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            // Edit Player's Queue
+            musicPlayerApplicationController.performQueueTransaction({ mutableQueue in
+                mutableQueue.removeItem(self.mediaItems[indexPath.row])
+            }, completionHandler: {queue, error in
+                self.mediaItems = queue.items
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            })
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+            
         }
+    }
+}
+
+// MARK: - MPMediaPickerControllerDelegate
+extension MasterViewController: MPMediaPickerControllerDelegate {
+    func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        // call private method
+        setQueueAndPlay(mediaItemCollection: mediaItemCollection)
+        
+        mediaPicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+        mediaPicker.dismiss(animated: true, completion: nil)
     }
 }
